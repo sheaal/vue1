24a8b713d61f7a35edfa7261f0b67c5ab36b48fc
sheaal <alisandra.sh05@gmail.com>
2024-02-09 18:59:57 +0700

    рендеринг списков

--- src/main.js
+++ src/main.js
@@ -6,7 +6,18 @@ let app = new Vue({
         image: "src/assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
         inStock: true,
-        inventory: 100,
-        onSale:"On Sale",
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+
     }
 })
